3818c596bff177ec28d2dc30120fc144cdd00fdf
nhann <nhan.nguyen@bkitsolution.com>
2023-10-05 14:45:43 +0700

    init

---
+++ src/main/java/com/example/demoloadapidocs/DemoLoadApiDocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoloadapidocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLoadApiDocsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoLoadApiDocsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/demoloadapidocs/DemoLoadApiDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoloadapidocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLoadApiDocsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
